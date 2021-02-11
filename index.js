c1e6f857c8880a667617a46fb8c660427e0084f1
albenchris <albenchris00@gmail.com>
2021-02-11 08:59:25 -0600

    added first question with command line functionality

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your application? (Required)'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(questions) {
+    return inquirer.prompt(questions);
+};
 
 // Function call to initialize app
-init();
+init(questions)
+    .then(answers => {
+        console.log(answers);
+    })
